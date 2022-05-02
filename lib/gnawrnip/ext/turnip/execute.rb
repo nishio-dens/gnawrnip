ef21bf2fda81367b60f2c23ad884388df0df3369
Shinsuke Nishio <nishio@densan-labs.net>
2022-05-02 22:02:55 +0900

    define_methodである必要はない

--- lib/gnawrnip/ext/turnip/execute.rb
+++ lib/gnawrnip/ext/turnip/execute.rb
@@ -2,7 +2,7 @@ module Turnip
   module Execute
     alias_method 'step_alias'.to_sym, 'step'
 
-    define_method 'step' do |*args, **kargs, &block|
+    def step(*args, **kargs, &block)
       Gnawrnip.photographer.take_shot
       send('step_alias', *args, **kargs, &block)
     end
