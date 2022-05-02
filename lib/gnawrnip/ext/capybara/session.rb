57cc29532604088afc529b519057bdfbd8a71c8b
Shinsuke Nishio <nishio@densan-labs.net>
2022-05-02 21:44:12 +0900

    ruby3対応

--- lib/gnawrnip/ext/capybara/session.rb
+++ lib/gnawrnip/ext/capybara/session.rb
@@ -2,31 +2,31 @@ require 'capybara/session'
 
 module Capybara
   class Session
-    SAVE_SCREENSHOT_METHODS = [
-      :click_button,
-      :click_link,
-      :click_link_or_button,
-      :click_on,
-      :fill_in,
-      :choose,
-      :check,
-      :uncheck,
-      :select,
-      :unselect,
-      :attach_file,
-      :execute_script,
-      :go_back,
-      :go_forward,
-      :switch_to_window,
-      :visit
+    SAVE_SCREENSHOT_METHODS = %i[
+      click_button
+      click_link
+      click_link_or_button
+      click_on
+      fill_in
+      choose
+      check
+      uncheck
+      select
+      unselect
+      attach_file
+      execute_script
+      go_back
+      go_forward
+      switch_to_window
+      visit
     ]
 
     SAVE_SCREENSHOT_METHODS.each do |method|
       alias_method "after_hook_#{method}".to_sym, method
 
-      define_method method do |*args, &block|
+      define_method method do |*args, **kwords, &block|
         Gnawrnip.photographer.take_shot if current_url != 'about:blank'
-        send("after_hook_#{method}", *args, &block)
+        send("after_hook_#{method}", *args, **kwords, &block)
       end
     end
   end
