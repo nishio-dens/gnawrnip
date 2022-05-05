1d172464a0c3c3c5f8819f0cafd1b5f16b1b38cc
Shinsuke Nishio <nishio@densan-labs.net>
2022-05-05 12:14:21 +0900

    kargs -> kwargs

--- lib/gnawrnip/ext/turnip/execute.rb
+++ lib/gnawrnip/ext/turnip/execute.rb
@@ -2,9 +2,9 @@ module Turnip
   module Execute
     alias_method 'step_alias'.to_sym, 'step'
 
-    def step(*args, **kargs, &block)
+    def step(*args, **kwargs, &block)
       Gnawrnip.photographer.take_shot
-      send('step_alias', *args, **kargs, &block)
+      send('step_alias', *args, **kwargs, &block)
     end
   end
 end
